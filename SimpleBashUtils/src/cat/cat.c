4b1103f58a9336c90709fad00f0450a14d8ae53e
nanadaug <nanadaug@student.21-school.ru>
2022-05-02 21:32:13 +0300

    Added new features

--- SimpleBashUtils/src/cat/cat.c
+++ SimpleBashUtils/src/cat/cat.c
@@ -19,57 +19,66 @@ int parse_flags(int *argc, char ***argv) {
     return flags;
 }
 
-void print_file(const char* file_name, int flags) {
+void print_file(FILE *file, int flags) {
     static short last_in_line_was_sim = 0;
     static short in_line_was_sim = 0;
 
     static unsigned long long int counter = 0;
 
-    FILE *file = fopen(file_name, "r");
-    if (file) {
-        char tmp;
-        // Read file
-        while ((tmp = fgetc(file)) != EOF) {
-            if (tmp == '\n') { // End line
-                if (flags &FLAG_S && (!in_line_was_sim && !last_in_line_was_sim))
-                    continue;
 
-                if (flags & FLAG_N && !(flags & FLAG_B) && !in_line_was_sim)
-                    printf("%6lld\t", ++counter);
+    char tmp;
+    // Read file
+    while ((tmp = fgetc(file)) != EOF) {
+        if (tmp == '\n') { // End line
+            if (flags &FLAG_S && (!in_line_was_sim && !last_in_line_was_sim))
+                continue;
 
-                if (flags & FLAG_E)
-                    putchar('$');
+            if (flags & FLAG_N && !(flags & FLAG_B) && !in_line_was_sim)
+                printf("%6lld\t", ++counter);
 
-                last_in_line_was_sim = in_line_was_sim;
-                in_line_was_sim = 0;
-            } else {
-                if (((flags & FLAG_B) || (flags & FLAG_N)) && !in_line_was_sim)
-                    printf("%6lld\t", ++counter);
-                in_line_was_sim = 1;
-            }
+            if (flags & FLAG_E)
+                putchar('$');
 
-            if ((flags & FLAG_T) && tmp == '\t') {
-                putchar('^');putchar('I');
-            } else {
-                putchar(tmp);
-            }
+            last_in_line_was_sim = in_line_was_sim;
+            in_line_was_sim = 0;
+        } else {
+            if (((flags & FLAG_B) || (flags & FLAG_N)) && !in_line_was_sim)
+                printf("%6lld\t", ++counter);
+            in_line_was_sim = 1;
         }
-        counter = 0;
-        in_line_was_sim = 0;
-        last_in_line_was_sim = 0;
-        fclose(file);
-    } else {
-        fprintf(stderr, "cat: %s: No such file or directory\n", file_name);
+
+        if ((flags & FLAG_T) && tmp == '\t') {
+            putchar('^');putchar('I');
+        } else {
+            putchar(tmp);
+        }
     }
+    counter = 0;
+    in_line_was_sim = 0;
+    last_in_line_was_sim = 0;
 }
 
 int main(int argc, char **argv) {
-    if (argc > 1) {
+    if (argc >= 1) {
         argv++, argc--;
         int flags = parse_flags(&argc, &argv);
 
+        if (!argc) {
+            print_file(stdin, flags);
+        }
+
         for (;argc && argv; argc--,argv++) {
-            print_file(*argv, flags);
+            if (!strcmp(*argv, "-")) {
+                print_file(stdin, flags);
+            } else {
+                FILE *file = fopen(*argv, "r");
+                if (file) {
+                    print_file(file, flags);
+                    fclose(file);
+                } else {
+                    fprintf(stderr, "cat: %s: No such file or directory\n", *argv);
+                }
+            }
         }
     }
     return 0;
