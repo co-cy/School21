655a015dfb2a2870f7b93b214599d7e10128c5f3
nanadaug <nanadaug@student.21-school.ru>
2022-05-13 16:17:33 +0000

    Remove trash

--- SimpleBashUtils/src/flag.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLEBASHUTILS_FLAG_H
-#define SIMPLEBASHUTILS_FLAG_H
-
-#define Iflag(flag) (1 << flag)
-#define add_flag(flags, flag) (flags |= Iflag(flag))
-#define check_flag(flags, flag) (flags & Iflag(flag))
-
-#endif //SIMPLEBASHUTILS_FLAG_H
