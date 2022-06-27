05394eaaa987efd1960949235b1b8386021e6811
nanadaug <nanadaug@student.21-school.ru>
2022-06-27 13:47:56 +0300

    Clone from off

---
+++ s21_calc/src/main.c
@@ -0,0 +1,9 @@
+//
+// Created by Nana Daughterless on 6/27/22.
+//
+
+#include "s21_calc.h"
+
+int main() {
+    return 0;
+}
---
+++ s21_calc/src/s21_calc.h
@@ -0,0 +1,8 @@
+//
+// Created by Nana Daughterless on 6/27/22.
+//
+
+#ifndef S21_CALC_S21_CALC_H
+#define S21_CALC_S21_CALC_H
+
+#endif //S21_CALC_S21_CALC_H
