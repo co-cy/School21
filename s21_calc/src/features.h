dd47a03275cdf2f6030476b7422a06f18a8d69ad
nanadaug <nanadaug@student.21-school.ru>
2022-06-27 16:42:47 +0300

    Added new features

---
+++ s21_calc/src/features.h
@@ -0,0 +1,14 @@
+//
+// Created by Nana Daughterless on 6/27/22.
+//
+
+#ifndef S21_CALC_FEATURES_H
+#define S21_CALC_FEATURES_H
+
+#define elif else if
+typedef enum {
+    False = 0,
+    True = 1,
+} bool;
+
+#endif //S21_CALC_FEATURES_H
