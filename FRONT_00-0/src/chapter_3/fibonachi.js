b4c197b9dec3a215cb15e723dd595f62a8fcaeb0
_co_cy_ <stf2003.kem@gmail.com>
2022-07-14 22:30:16 +0300

    [day0] -> Completed task 3.4

--- FRONT_00-0/src/chapter_3/fibonachi.js
+++ FRONT_00-0/src/chapter_3/fibonachi.js
@@ -3,8 +3,14 @@
 
 
 function fibo(index) {
-
+    if (index === 0 || index === 1) {
+        return 1;
+    } else {
+        return fibo(index - 1) + fibo(index - 2);
+    }
 }
 
-
-fibo(5) // Вернет 8 
+console.log(fibo(5))  // Вернет 8
+console.log(fibo(1))  // Вернет 1
+console.log(fibo(8))  // Вернет 34
+console.log(fibo(21))  // Вернет 1771
