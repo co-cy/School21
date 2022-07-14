44cc4cb69301a2d4101e5631a340aceab6542185
_co_cy_ <stf2003.kem@gmail.com>
2022-07-14 22:57:03 +0300

    [day0] -> Completed task 4.7

--- FRONT_00-0/src/chapter_4/atm.js
+++ FRONT_00-0/src/chapter_4/atm.js
@@ -4,11 +4,34 @@
 // За раз банкомат может выдавать не более 20 купюр, если купюр для выдачи не хватает то выводится ошибка 'Limit exceeded'
 
 function atm(sum) {
-  const banknots = [5000, 2000, 1000, 500, 200, 100, 50];
+    const banknots = [5000, 2000, 1000, 500, 200, 100, 50];
+    let result = {};
+
+    let count_banknots = 0;
+
+    for (let banknot of banknots) {
+        result[banknot] = 0;
+        while (banknot <= sum) {
+            result[banknot] += 1;
+            sum -= banknot;
+            if (++count_banknots > 20) {
+                return "Limit exceeded"
+            }
+        }
+    }
+
+    if (sum) {
+        return 'Incorrect value';
+    } else {
+        return result;
+    }
 }
 
-atm(8350); // {5000 : 1, 2000 : 1, 1000 : 1, 200 : 1, 100 : 1, 50 : 1 }
-atm(2570); // Incorrect value
-atm(100050); // limit exceeded
+
+console.log(atm(8350)); // {5000 : 1, 2000 : 1, 1000 : 1, 200 : 1, 100 : 1, 50 : 1 }
+console.log(atm(8300)); // {5000 : 1, 2000 : 1, 1000 : 1, 200 : 1, 100 : 1, 50 : 1 }
+console.log(atm(20000)); // {5000 : 1, 2000 : 1, 1000 : 1, 200 : 1, 100 : 1, 50 : 1 }
+console.log(atm(2570)); // Incorrect value
+console.log(atm(100050)); // limit exceeded
 
 
