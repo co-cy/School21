f4ead7013f4f8f0d1536cfc18c8422e4f9a43ad2
_co_cy_ <stf2003.kem@gmail.com>
2022-07-14 22:40:16 +0300

    [day0] -> Completed task 4.5

--- FRONT_00-0/src/chapter_4/removeReps.js
+++ FRONT_00-0/src/chapter_4/removeReps.js
@@ -1,6 +1,24 @@
 // Вам нужно написать функцию которая принимает в кач-ве аргумента массива чисел и удаляет все повторяющиеся значения
 
-function removeReps(array) {}
+function removeReps(array) {
+    let new_array = [];
+    for (let i = 0; i < array.length; i++) {
+        let j;
+        for (j = 0; j < new_array.length; j++) {
+            if (array[i] === new_array[j]) {
+                break
+            }
+        }
 
-removeReps([1, 1, 2, 4, 5, 6, 6, 8, 9, 11]); // [1,2,4,5,6,8,9,11]
+        if (j >= new_array.length || array[i] !== new_array[j]) {
+            new_array.push(array[i]);
+        }
+    }
+
+    return new_array;
+}
+
+console.log(removeReps([1, 1, 2, 4, 5, 6, 6, 8, 9, 11])); // [1,2,4,5,6,8,9,11]
+console.log(removeReps([1,1,1,1])); // [1]
+console.log(removeReps([1,2,3,4,5,6])); // [1,2,3,4,5,6]
 
