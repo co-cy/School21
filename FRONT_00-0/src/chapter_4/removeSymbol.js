00e81225dd7dbdfa0801d3403c501ed9f6a0eb96
_co_cy_ <stf2003.kem@gmail.com>
2022-07-14 22:47:25 +0300

    [day0] -> Completed task 4.6

--- FRONT_00-0/src/chapter_4/removeSymbol.js
+++ FRONT_00-0/src/chapter_4/removeSymbol.js
@@ -1,8 +1,12 @@
 // Функция на вход принимает две строки - сообщение (обычная строка с текстом) и символ который надо удалить из этого сообщения
 
-function removeString(message, symbol) {}
+function removeString(message, symbol) {
+    return message.replace(new RegExp(symbol, "g"), "").trim()
+}
 
-removeString("Большое и интересное сообщение", "о"); // Бльше и интересне сбщение
+console.log(removeString("Большое и интересное сообщение", "о")); // Бльше и интересне сбщение
+console.log(removeString("Hello world!", "z")); // Hello world!
+console.log(removeString("А роза азора", "А")); // роза азора
 
 
 
