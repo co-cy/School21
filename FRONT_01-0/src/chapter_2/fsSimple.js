44367f2cd20d5fa0f7f37339c4a394dc5042235d
_co_cy_ <stf2003.kem@gmail.com>
2022-07-17 12:26:46 +0300

    [day2] -> Completed task 3 Day01

--- FRONT_01-0/src/chapter_2/fsSimple.js
+++ FRONT_01-0/src/chapter_2/fsSimple.js
@@ -1,13 +1,25 @@
-const fs = requere("fs");
+const fs = require("fs");
 
-const readAndWriteCallbackHell = () => {};
+const readAndWriteCallbackHell = () => {
+  fs.readFile("files/fsSimple/file1.txt", (err, data) => {
+    if (err)
+      throw err;
+    fs.writeFile("files/fsSimple/file2.txt", data, (err) => {
+      if (err)
+        throw err;
+      console.log("Save!");
+    });
+  })
+};
 
-const readAndWritePromises = () => {};
+const readAndWritePromises = () => {
+  let promise = fs.promises.readFile("files/fsSimple/file1.txt");
+  promise.then((res) => {
+    fs.promises.writeFile("files/fsSimple/file2.txt", res);
+  })
+};
 
-const readAndWriteAsyncAwait = async () => {};
-
-export {
-  readAndWriteAsyncAwait,
-  readAndWritePromises,
-  readAndWriteCallbackHell,
+const readAndWriteAsyncAwait = async () => {
+  let res = await fs.promises.readFile("files/fsSimple/file1.txt");
+  fs.promises.writeFile("files/fsSimple/file2.txt", res);
 };
